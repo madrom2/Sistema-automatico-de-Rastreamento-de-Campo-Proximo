7638c60dea593f5250a43fa9c50d030c7d424e8f
André Luiz <53880749+madrom2@users.noreply.github.com>
2021-07-01 03:28:48 -0300

    comunicação com analisador

---
+++ application_gui/analisador_controle.py
@@ -0,0 +1,46 @@
+import pyvisa
+import serial
+
+class controle_analisador:
+    def open_visa_analisador(com_port, visa_analisador):
+        if (visa_analisador == None):
+            #retirar numero da COM 
+            for i in range (len(com_port)):
+                if com_port[i].isnumeric():
+                    break
+            com_port = com_port[i:int(com_port.find(' -'))]
+            #Inicialização da COM
+            rm = pyvisa.ResourceManager()
+            #Se não tiver uma porta COM compativel instrumento retorna False
+            #para apresentação de erro,  falta de drive
+            if not any(str(com_port) in i for i in rm.list_resources()):
+                return False
+            else:
+                try:
+                    my_instrument = rm.open_resource('ASRL'+str(com_port)+'::INSTR')
+                except Exception as e:
+                    print(e)
+                    my_instrument.close()
+                    return None
+                #inicialização do instrumento
+                my_instrument.write('*RST;*CLS')     #reset no instrumento e limpa erro
+                my_instrument.write('SYST:MODE RMOD')#ativa modo reciver
+                
+                return my_instrument
+        else:
+            visa_analisador.close()
+            return None
+        
+    #Função de indentificação
+    def identificação(visa_analisador):
+        return visa_analisador.query('*IDN?')
+        
+    #Função definição da frequencia em Hz
+    def receiver_frequencia(visa_analisador,freq):
+        visa_analisador.write('RMOD:FREQ {}'.format(freq))
+        
+    #Função leitura da amplitude
+    def receiver_amplitude(visa_analisador):
+        return eval(my_instrument.query('RMODe:LEV?').split(',')[1])
+        
+        
